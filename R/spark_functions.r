e74abbe7239e9040bf82fed82ff3e53235dc638c
tito_am <fabian.arandia@gmail.com>
2020-08-31 18:12:59 -0400

    reajustement en fonctions

--- R/spark_functions.r
+++ R/spark_functions.r
@@ -1,111 +1,127 @@
 
 
-library(tidyverse)
-library(readxl)
-library(reshape2)
-rm(list=ls())
 
-##########################################################
-#Preparation des fichiers pour spark
-#aller lire les fichiers dans le repertoire pour hecressim
-mainDir<-'/media/tito/TIIGE/PRSIM/0.9995/bv_csv_hecressim'
-setwd(mainDir)
+#' ecriture_prsim_vers_csv
+#'
+#' @param path 
+#'
+#' @return
+#' @export
+#'
+#' @examples
+#' mainDir<-'/media/tito/TIIGE/PRSIM/0.9995/bv_csv_hecressim'
 
-bvs_temiscamingue<-c( "Dozois","Lac Victoria et lac Granet","Rapide-7","Rapide-2","Riviere Kinojevis","Lac des Quinze","Mistinikon","Lady Evelyn","Lower Notch et Indian Chute","Rabbit Lake", "Kipawa",
-                      "Lac Temiscamingue a Angliers","Riviere Blanche" )
-
-
-mainDir2<-'/media/tito/TIIGE/PRSIM/0.9995/'
-subDir<-'temiscamingue_sum'
-dir.create(file.path(mainDir2, subDir), showWarnings = FALSE)
-
-fichiers<-list.files()
-
-for(fichier in fichiers){ 
-  df<-read_csv(fichier)
-  df<-df[bvs_temiscamingue]
-  temiscamingue_row_sum<-rowSums(df)
-  plot(temiscamingue_row_sum,type='l')
-  filename<-paste0(mainDir2,subDir,'/temiscamingue_sum_',fichier)
-  df<-as.data.frame(cbind(rep(fichier,length(temiscamingue_row_sum)),seq(1,length(temiscamingue_row_sum)),temiscamingue_row_sum))
-  colnames(df)<-c('sim_number','julian_day','temiscamingue_sum')
-  write_csv(as.data.frame(df),filename)
+ecriture_prsim_vers_csv<-function(path){
+  require(tidyverse)
+  require(readxl)
+  require(reshape2)
+  bvs_temiscamingue<-c( "Dozois","Lac Victoria et lac Granet","Rapide-7","Rapide-2","Riviere Kinojevis","Lac des Quinze","Mistinikon","Lady Evelyn","Lower Notch et Indian Chute","Rabbit Lake", "Kipawa",
+                        "Lac Temiscamingue a Angliers","Riviere Blanche" )
+  
+  
+  mainDir2<-'/media/tito/TIIGE/PRSIM/0.9995/'
+  subDir<-'temiscamingue_sum'
+  dir.create(file.path(mainDir2, subDir), showWarnings = FALSE)
+  
+  fichiers<-list.files()
+  
+  for(fichier in fichiers){ 
+    df<-read_csv(fichier)
+    df<-df[bvs_temiscamingue]
+    temiscamingue_row_sum<-rowSums(df)
+    plot(temiscamingue_row_sum,type='l')
+    filename<-paste0(mainDir2,subDir,'/temiscamingue_sum_',fichier)
+    df<-as.data.frame(cbind(rep(fichier,length(temiscamingue_row_sum)),seq(1,length(temiscamingue_row_sum)),temiscamingue_row_sum))
+    colnames(df)<-c('sim_number','julian_day','temiscamingue_sum')
+    write_csv(as.data.frame(df),filename)
+  }
+  
 }
 
-#lecture des csv des hydrogrammes a carillon
-#strategie: 1) avoir les quantiles de l'hydrogramme final 2) calculer les quantiles de pointes
 
-library(sparklyr)
-library(dplyr)
-library(tidyr)
+#' calcul_prsim_vers_statistiques_sommaires_temiscamingue
+#'
+#' @param path 
+#'
+#' @return
+#' @export
+#'
+#' @examples
+calcul_prsim_vers_statistiques_sommaires_temiscamingue<-function(path){
+  
+  require(sparklyr)
+  require(dplyr)
+  require(tidyr)
+  
+  config <- spark_config()
+  
+  config$`sparklyr.shell.driver-memory` <- "2G"
+  config$`sparklyr.shell.executor-memory` <- "2G"
+  config$`spark.yarn.executor.memoryOverhead` <- "512"
+  
+  # Connect to local cluster with custom configuration
+  sc <- spark_connect(master = "local", config = config)
+  
+  spec_with_r <- sapply(read.csv('/media/tito/TIIGE/PRSIM/0.9995/temiscamingue_sum/temiscamingue_sum_0000001.csv', nrows = 1), class)
+  
+  testo<-spark_read_csv(sc = sc,path = paste0(mainDir2,subDir),columns=spec_with_r,memory = FALSE)
+  src_tbls(sc)
+  testo$ops$vars
+  #testo %>% filter(carillon_sum > 2000)
+  
+  df_mean_per_julian_day = testo %>% group_by(julian_day) %>% summarise(AvgQ=mean(temiscamingue_sum))%>% collect()
+  df_max_per_julian_day = testo %>% group_by(julian_day) %>% summarise(MaxQ=max(temiscamingue_sum))%>% collect()
+  df_min_per_julian_day = testo %>% group_by(julian_day) %>% summarise(MinQ=min(temiscamingue_sum))%>% collect()
+  
+  #df_max_per_julian_day = testo %>% summarise(MaxQ=max(temiscamingue_sum))%>% collect()
+  
+  
+  #mettre en ordre les statistiques sommaires de l'hydrogramme
+  df_mean_per_julian_day_ordered<-df_mean_per_julian_day[order(df_mean_per_julian_day$julian_day),]
+  df_max_per_julian_day_ordered<-df_max_per_julian_day[order(df_max_per_julian_day$julian_day),]
+  df_min_per_julian_day_ordered<-df_min_per_julian_day[order(df_min_per_julian_day$julian_day),]
+  
+  #ggplot des statistiques sommaires des simulations PRSIM
+  plot(df_max_per_julian_day_ordered,type='l',ylim=c(0,10000))                
+  points(df_mean_per_julian_day_ordered,type='l')
+  points(df_min_per_julian_day_ordered,type='l')
+  #ajout des informations de debit de la premiere etude
+  points(qobs_mean_per_day$AvgQ,type='l',col='red')                
+  points(qobs_max_per_day$MaxQ,type='l',col='red')
+  points(qobs_min_per_day$MinQ,type='l',col='red')
+  #sdf_pivot(testo, sim_number ~ carillon_sum)
+  
+  #calcul de la pointe a carillon
+  #ajouter les saisons
+  #res=testo%>%filter(season %in% target)%>%group_by(sim_number)%>%summarize(max=max(carillon_sum))%>%collect()
+  
+  #maximun par annee
+  res=testo%>%group_by(sim_number)%>%summarize(max=max(temiscamingue_sum))%>%collect()
+  #ecdf cunnane
+  ecdf_cunnane<-function (x) 
+  {
+    x <- sort(x)
+    n <- length(x)
+    if (n < 1) 
+      stop("'x' must have 1 or more non-missing values")
+    vals <- unique(x)
+    rval <- approxfun(vals, cumsum(tabulate(match(x, vals))-0.4)/(n+0.2), 
+                      method = "constant", yleft = 0, yright = 1, f = 0, ties = "ordered")
+    class(rval) <- c("ecdf", "stepfun", class(rval))
+    assign("nobs", n, envir = environment(rval))
+    attr(rval, "call") <- sys.call()
+    rval
+  }
+  ecdf_max_year<-ecdf_cunnane(res$max)
+  plot(ecdf_max_year)
+  Fn<- ecdf_cunnane(res$max)
+  #prendre les codes pour cunnane
+  quantiles_qinter<-data.frame(quantiles=quantile(Fn, prob = c((1-(1/10000)),(1-(1/2000)),(1-(1/1000)),(1-(1/200)),(1-(1/100)),(1-(1/50)),(1-(1/20)),(1-(1/10)),(1-(1/2))), names = FALSE),row.names=c(10000,2000,1000,200,100,50,20,10,2))
+  
+  quantiles_qinter_2<-data.frame(quantiles=quantile(ecdf_max_year, prob = c((1-(1/10000)),(1-(1/2000)),(1-(1/1000)),(1-(1/200)),(1-(1/100)),(1-(1/50)),(1-(1/20)),(1-(1/10)),(1-(1/2))), names = FALSE),row.names=c(10000,2000,1000,200,100,50,20,10,2))
+  
+  #
+  
+  spark_disconnect(sc)
 
-config <- spark_config()
-
-config$`sparklyr.shell.driver-memory` <- "2G"
-config$`sparklyr.shell.executor-memory` <- "2G"
-config$`spark.yarn.executor.memoryOverhead` <- "512"
-
-# Connect to local cluster with custom configuration
-sc <- spark_connect(master = "local", config = config)
-
-spec_with_r <- sapply(read.csv('/media/tito/TIIGE/PRSIM/0.9995/temiscamingue_sum/temiscamingue_sum_0000001.csv', nrows = 1), class)
-
-testo<-spark_read_csv(sc = sc,path = paste0(mainDir2,subDir),columns=spec_with_r,memory = FALSE)
-src_tbls(sc)
-testo$ops$vars
-#testo %>% filter(carillon_sum > 2000)
-
-df_mean_per_julian_day = testo %>% group_by(julian_day) %>% summarise(AvgQ=mean(temiscamingue_sum))%>% collect()
-df_max_per_julian_day = testo %>% group_by(julian_day) %>% summarise(MaxQ=max(temiscamingue_sum))%>% collect()
-df_min_per_julian_day = testo %>% group_by(julian_day) %>% summarise(MinQ=min(temiscamingue_sum))%>% collect()
-
-#df_max_per_julian_day = testo %>% summarise(MaxQ=max(temiscamingue_sum))%>% collect()
-
-
-#mettre en ordre les statistiques sommaires de l'hydrogramme
-df_mean_per_julian_day_ordered<-df_mean_per_julian_day[order(df_mean_per_julian_day$julian_day),]
-df_max_per_julian_day_ordered<-df_max_per_julian_day[order(df_max_per_julian_day$julian_day),]
-df_min_per_julian_day_ordered<-df_min_per_julian_day[order(df_min_per_julian_day$julian_day),]
-
-#ggplot des statistiques sommaires des simulations PRSIM
-plot(df_max_per_julian_day_ordered,type='l',ylim=c(0,10000))                
-points(df_mean_per_julian_day_ordered,type='l')
-points(df_min_per_julian_day_ordered,type='l')
-#ajout des informations de debit de la premiere etude
-points(qobs_mean_per_day$AvgQ,type='l',col='red')                
-points(qobs_max_per_day$MaxQ,type='l',col='red')
-points(qobs_min_per_day$MinQ,type='l',col='red')
-#sdf_pivot(testo, sim_number ~ carillon_sum)
-
-#calcul de la pointe a carillon
-#ajouter les saisons
-#res=testo%>%filter(season %in% target)%>%group_by(sim_number)%>%summarize(max=max(carillon_sum))%>%collect()
-
-#maximun par annee
-res=testo%>%group_by(sim_number)%>%summarize(max=max(temiscamingue_sum))%>%collect()
-#ecdf cunnane
-ecdf_cunnane<-function (x) 
-{
-  x <- sort(x)
-  n <- length(x)
-  if (n < 1) 
-    stop("'x' must have 1 or more non-missing values")
-  vals <- unique(x)
-  rval <- approxfun(vals, cumsum(tabulate(match(x, vals))-0.4)/(n+0.2), 
-                    method = "constant", yleft = 0, yright = 1, f = 0, ties = "ordered")
-  class(rval) <- c("ecdf", "stepfun", class(rval))
-  assign("nobs", n, envir = environment(rval))
-  attr(rval, "call") <- sys.call()
-  rval
 }
-ecdf_max_year<-ecdf_cunnane(res$max)
-plot(ecdf_max_year)
-Fn<- ecdf_cunnane(res$max)
-#prendre les codes pour cunnane
-quantiles_qinter<-data.frame(quantiles=quantile(Fn, prob = c((1-(1/10000)),(1-(1/2000)),(1-(1/1000)),(1-(1/200)),(1-(1/100)),(1-(1/50)),(1-(1/20)),(1-(1/10)),(1-(1/2))), names = FALSE),row.names=c(10000,2000,1000,200,100,50,20,10,2))
-
-quantiles_qinter_2<-data.frame(quantiles=quantile(ecdf_max_year, prob = c((1-(1/10000)),(1-(1/2000)),(1-(1/1000)),(1-(1/200)),(1-(1/100)),(1-(1/50)),(1-(1/20)),(1-(1/10)),(1-(1/2))), names = FALSE),row.names=c(10000,2000,1000,200,100,50,20,10,2))
-
-#
-
-spark_disconnect(sc)
