bd342b76dd1754f624c9e5e8b991120ff8875139
tito_am <fabian.arandia@gmail.com>
2020-09-11 10:20:23 -0400

    Comparaison des hydrogrammes de l'étude de 2019 et prsim

--- R/hydro_sum_bonifie_compare.r
+++ R/hydro_sum_bonifie_compare.r
@@ -52,3 +52,55 @@ Comparaison_Temiscamingue<-function(path){
 
 }
 
+
+#' Comparaison de la somme des apports intermediaires avec les simulations de prsim
+#'
+#' @param path
+#'
+#' @return
+#' @export
+#'
+#' @examples
+#' path<-'/home/tito/Documents/github/prsim/outaouais_sup_lynda/Frequentielles/Apports Latéraux Methode extrême PRT/Q10000/'
+#' sum_angliers<-Comparaison_Angliers(path)
+
+Comparaison_Angliers<-function(path){
+
+  require(readxl)
+  require(tidyverse)
+  require(readr)
+
+
+
+
+  setwd(path)
+
+  files<-list.files(pattern = '.csv')
+
+
+  total_angliers<-list()
+
+  for(file in files){
+
+    df<-readr::read_csv(file)
+
+    #somme jusqu'a temiscamingue
+
+    bvs_angliers<-c( "Dozois","Lac Victoria et lac Granet","Rapide 7","Rapide 2","Riviere Kinojevis","Lac des Quinze")
+
+    x<-df[bvs_angliers]
+
+    sum_angliers<-rowSums (x, na.rm = FALSE, dims = 1)
+
+    sum_carillon<-rowSums (df, na.rm = FALSE, dims = 1)
+
+    total_angliers[[file]]<-sum_angliers
+  }
+
+  df_test<-do.call(cbind,total_angliers)
+
+  matplot(df_test,type='l',col='blue',lty=1,xlab = 'jours juliens',ylab='Q', main="Scénarios de crue 1:10000 à Angliers de l''étude de l''Outaouais Supérieur")
+  grid()
+  return(sum_angliers)
+
+}
--- R/spark_functions.r
+++ R/spark_functions.r
@@ -11,7 +11,44 @@
 #' @examples
 #' mainDir<-'/media/tito/TIIGE/PRSIM/0.9995/bv_csv_hecressim'
 
-ecriture_prsim_vers_csv<-function(path){
+ecriture_prsim_vers_csv_angliers<-function(path){
+  require(tidyverse)
+  require(readxl)
+  require(reshape2)
+  bvs_angliers<-c("Dozois","Lac Victoria et lac Granet","Rapide-7","Rapide-2","Riviere Kinojevis","Lac des Quinze")
+
+
+  mainDir2<-'/media/tito/TIIGE/PRSIM/0.9995/'
+  subDir<-'angliers_sum'
+  dir.create(file.path(mainDir2, subDir), showWarnings = FALSE)
+  setwd(path)
+  fichiers<-list.files()
+
+  for(fichier in fichiers){
+    df<-read_csv(fichier)
+    df<-df[bvs_angliers]
+    angliers_row_sum<-rowSums(df)
+    plot(angliers_row_sum,type='l')
+    filename<-paste0(mainDir2,subDir,'/angliers_sum_',fichier)
+    df<-as.data.frame(cbind(rep(fichier,length(angliers_row_sum)),seq(1,length(angliers_row_sum)),angliers_row_sum))
+    colnames(df)<-c('sim_number','julian_day','angliers_sum')
+    write_csv(as.data.frame(df),filename)
+  }
+
+}
+
+
+#' ecriture_prsim_vers_csv_temiscamingue
+#'
+#' @param path
+#'
+#' @return
+#' @export
+#'
+#' @examples
+#' mainDir<-'/media/tito/TIIGE/PRSIM/0.9995/bv_csv_hecressim'
+
+ecriture_prsim_vers_csv_temiscamingue<-function(path){
   require(tidyverse)
   require(readxl)
   require(reshape2)
@@ -22,7 +59,7 @@ ecriture_prsim_vers_csv<-function(path){
   mainDir2<-'/media/tito/TIIGE/PRSIM/0.9995/'
   subDir<-'temiscamingue_sum'
   dir.create(file.path(mainDir2, subDir), showWarnings = FALSE)
-
+  setwd(path)
   fichiers<-list.files()
 
   for(fichier in fichiers){
@@ -47,7 +84,7 @@ ecriture_prsim_vers_csv<-function(path){
 #' @export
 #'
 #' @examples
-calcul_prsim_vers_statistiques_sommaires_temiscamingue<-function(mainDir2){
+calcul_prsim_vers_statistiques_sommaires_angliers<-function(mainDir2){
 
   require(sparklyr)
   require(dplyr)
@@ -55,35 +92,37 @@ calcul_prsim_vers_statistiques_sommaires_temiscamingue<-function(mainDir2){
 
   config <- spark_config()
 
-  config$`sparklyr.shell.driver-memory` <- "2G"
-  config$`sparklyr.shell.executor-memory` <- "2G"
+  config$`sparklyr.shell.driver-memory` <- "4G"
+  config$`sparklyr.shell.executor-memory` <- "4G"
   config$`spark.yarn.executor.memoryOverhead` <- "512"
 
   # Connect to local cluster with custom configuration
   sc <- spark_connect(master = "local", config = config)
 
-  spec_with_r <- sapply(read.csv('/media/tito/TIIGE/PRSIM/0.9995/temiscamingue_sum/temiscamingue_sum_0000001.csv', nrows = 1), class)
+  spec_with_r <- sapply(read.csv('/media/tito/TIIGE/PRSIM/0.9995/angliers_sum/angliers_sum_0000001.csv', nrows = 1), class)
 
-  subDir<-'temiscamingue_sum'
+  subDir<-'angliers_sum'
   testo<-spark_read_csv(sc = sc,path = paste0(mainDir2,subDir),columns=spec_with_r,memory = FALSE)
   src_tbls(sc)
   testo$ops$vars
   #testo %>% filter(carillon_sum > 2000)
-
-  df_mean_per_julian_day = testo %>% group_by(julian_day) %>% summarise(AvgQ=mean(temiscamingue_sum,na.rm = TRUE))%>% collect()
-  df_max_per_julian_day = testo %>% group_by(julian_day) %>% summarise(MaxQ=max(temiscamingue_sum,na.rm = TRUE))%>% collect()
-  df_min_per_julian_day = testo %>% group_by(julian_day) %>% summarise(MinQ=min(temiscamingue_sum,na.rm = TRUE))%>% collect()
+  start_time = Sys.time()
+  df_mean_per_julian_day = testo %>% group_by(julian_day) %>% summarise(AvgQ=mean(angliers_sum,na.rm = TRUE))%>% collect()
+  end_time = Sys.time()
+  end_time - start_time
+  df_max_per_julian_day = testo %>% group_by(julian_day) %>% summarise(MaxQ=max(angliers_sum,na.rm = TRUE))%>% collect()
+  df_min_per_julian_day = testo %>% group_by(julian_day) %>% summarise(MinQ=min(angliers_sum,na.rm = TRUE))%>% collect()
 
   #mettre en ordre les statistiques sommaires de l'hydrogramme
   df_mean_per_julian_day_ordered<-df_mean_per_julian_day[order(df_mean_per_julian_day$julian_day),]
   df_max_per_julian_day_ordered<-df_max_per_julian_day[order(df_max_per_julian_day$julian_day),]
   df_min_per_julian_day_ordered<-df_min_per_julian_day[order(df_min_per_julian_day$julian_day),]
 
-  final_prsim_temiscamingue<-c(df_mean_per_julian_day_ordered,df_max_per_julian_day_ordered,df_min_per_julian_day_ordered)
-  save(final_prsim_temiscamingue,file='~/Documents/github/prsim/outaouais_sup_lynda/final_prsim_temiscamingue.RData')
+  final_prsim_angliers<-c(df_mean_per_julian_day_ordered,df_max_per_julian_day_ordered,df_min_per_julian_day_ordered)
+  save(final_prsim_angliers,file='~/Documents/github/prsim/outaouais_sup_lynda/final_prsim_angliers.RData')
 
   #ggplot des statistiques sommaires des simulations PRSIM
-  plot(df_max_per_julian_day_ordered,type='l',ylim=c(0,10000))
+  plot(df_max_per_julian_day_ordered,type='l',ylim=c(0,4000))
   points(df_mean_per_julian_day_ordered,type='l')
   points(df_min_per_julian_day_ordered,type='l')
   #ajout des informations de debit de la premiere etude
@@ -97,7 +136,7 @@ calcul_prsim_vers_statistiques_sommaires_temiscamingue<-function(mainDir2){
   #res=testo%>%filter(season %in% target)%>%group_by(sim_number)%>%summarize(max=max(carillon_sum))%>%collect()
 
   #maximun par annee
-  res=testo%>%group_by(sim_number)%>%summarize(max=max(temiscamingue_sum))%>%collect()
+  res=testo%>%group_by(sim_number)%>%summarize(max=max(angliers_sum))%>%collect()
   #ecdf cunnane
   ecdf_cunnane<-function (x)
   {
@@ -122,7 +161,7 @@ calcul_prsim_vers_statistiques_sommaires_temiscamingue<-function(mainDir2){
   quantiles_qinter_2<-data.frame(quantiles=quantile(ecdf_max_year, prob = c((1-(1/10000)),(1-(1/2000)),(1-(1/1000)),(1-(1/200)),(1-(1/100)),(1-(1/50)),(1-(1/20)),(1-(1/10)),(1-(1/2))), names = FALSE),row.names=c(10000,2000,1000,200,100,50,20,10,2))
 
   #
-  res<-c(final_prsim_temiscamingue,quantiles_qinter)
+  res<-c(final_prsim_angliers,quantiles_qinter)
   spark_disconnect(sc)
 
   return(res)
